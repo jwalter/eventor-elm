3ee5b4fecec57e0897a07758be1121eee09b664c
jwalter <joakim.waltersson@gmail.com>
2016-07-10 09:53:25 +0200

    Include all races for multi-day competitions and sort by date

--- src/Competitions/List.elm
+++ src/Competitions/List.elm
@@ -3,6 +3,7 @@ module Competitions.List exposing (..)
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (class, href, style)
+import String
 import Hop.Types exposing (Location)
 import Competitions.Models exposing (..)
 import Competitions.Messages exposing (..)
@@ -24,19 +25,42 @@ styles =
 
 view : ViewModel -> Html Msg
 view model =
-    div [ styles ]
-        [ h2 [] [ text "Tävlingar" ]
-        , table []
-            [ tbody [] (tableRows model.competitions) ]
-        ]
+    let
+        races =
+            model.competitions
+                |> allRaces
+                |> List.sortBy (\c -> c.startDate)
+    in
+        div [ styles ]
+            [ h2 [] [ text "Tävlingar" ]
+            , table []
+                [ tbody [] (tableRows races) ]
+            ]
 
 
-tableRows : List Competition -> List (Html Msg)
+allRaces : List Competition -> List Race
+allRaces competitions =
+    List.foldr (\c all -> (List.append all (List.map (mergeNames c) c.races))) [] competitions
+
+
+mergeNames : Competition -> Race -> Race
+mergeNames competition race =
+    let
+        name =
+            if String.length race.name > 0 then
+                competition.name ++ ", " ++ race.name
+            else
+                competition.name
+    in
+        { race | name = name }
+
+
+tableRows : List Race -> List (Html Msg)
 tableRows collection =
     List.map rowView collection
 
 
-rowView : Competition -> Html Msg
+rowView : Race -> Html Msg
 rowView competition =
     tr []
         [ td [ onClick (Show competition.id) ]
